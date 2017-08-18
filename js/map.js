09a673db6dbedbcc0219ecdbfb6ba3f91820c81e
Sadovenko Dmitriy <sadovenko.dmitry@gmail.com>
2017-08-18 14:01:59 +0300

    Доделано задание личного проекта

--- js/map.js
+++ js/map.js
@@ -46,14 +46,18 @@ var getAnyElement = function (arrayOfElements) {
 var getRandomNumber = function (max, min) {
   return Math.round(Math.random() * (max - min) + min);
 };
-
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 // Переменные
+var pinSize = {
+  width: 40,
+  height: 40
+};
+
 function Ads() {
   // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-  // Локальные переменные, относящиеся к обявлению
+  // Локальные переменные и методы экземпляра обявления
   var userAvatarPaths = {
     values: generateAvatarImgPath(8),
     counter: 0
@@ -64,7 +68,10 @@ function Ads() {
     counter: 0
   };
 
-  var flatTypes = ['flat', 'house', 'bungalo'];
+  var flatTypes = {
+    en: ['flat', 'house', 'bungalo'],
+    rus: ['Квартира', 'Дом', 'Бунгало']
+  };
 
   var checkInOutTimes = ['12:00', '13:00', '14:00'];
 
@@ -72,28 +79,9 @@ function Ads() {
     values: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
     counter: 0
   };
-  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-  // Интерфейс объекта. Свойства
-  this.author = getAnyElementFisher(userAvatarPaths);
-  this.location = {x: getRandomNumber(900, 300), y: getRandomNumber(100, 500)};
-  this.offer = {
-    title: getAnyElementFisher(offerTitles),
-    address: this.location.x + ', ' + this.location.y,
-    price: getRandomNumber(1000000, 1000),
-    type: getAnyElement(flatTypes),
-    rooms: getRandomNumber(5, 1),
-    checkin: getAnyElement(checkInOutTimes),
-    checkout: getAnyElement(checkInOutTimes),
-    features: getFeaturesArray(additionalFeatures)
-  };
-  this.offer.guests = this.offer.rooms * getRandomNumber(3, 1);
-  this.description = '';
-  this.photos = [];
-
-  /**
-   * Возвращает массив случайно длины, содержащий доп. опции
+    /**
+   * Возвращает массив случаной длины, содержащий доп. опции
    * @param {Object} arrayOfElements - полный массив доп. опций
    * @return {Array}
    */
@@ -104,10 +92,94 @@ function Ads() {
       array.push(getAnyElementFisher(arrayOfElements));
     }
     return array;
-  }
+  };
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // Интерфейс объекта. Свойства
+  this.author = {avatar: getAnyElementFisher(userAvatarPaths)};
+  this.location = {x: getRandomNumber(900, 300), y: getRandomNumber(100, 500)};
+  this.offer = {
+    title: getAnyElementFisher(offerTitles),
+    address: this.location.x + ', ' + this.location.y,
+    price: getRandomNumber(1000000, 1000),
+    type: getAnyElement(flatTypes.en),
+    rooms: getRandomNumber(5, 1),
+    checkin: getAnyElement(checkInOutTimes),
+    checkout: getAnyElement(checkInOutTimes),
+    features: getFeaturesArray(additionalFeatures)
+  };
+  this.offer.guests = this.offer.rooms * getRandomNumber(3, 1);
+  this.description = '';
+  this.photos = [];
+
+  // Интерфейс объекта. Методы
+  /**
+   * Возвращает кириллическое название типа помещения
+   * @param {string} offerTypeEn - англоязычное названия типа помещения
+   * @return {*}
+   */
+  this.getRusLodgeType = function (offerTypeEn) {
+    return flatTypes.rus[flatTypes.en.indexOf(offerTypeEn)];
+  };
 }
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-var ads = new Ads();
+var i = 0;
+var arrayOfAds = [];
+var fragment = document.createDocumentFragment();
+var templateOffer = document.getElementById('lodge-template').content;
+
+for (i = 0; i < 8; i++) {
+  // Генерация массива объявляений
+  arrayOfAds.push(new Ads());
+
+  // Создание div-блока для нового флажка
+  var newDiv = document.createElement('div');
+  newDiv.className = 'pin';
+  newDiv.setAttribute('style', 'left: ' + (arrayOfAds[i].location.x + pinSize.width / 2) + 'px; top: ' + (arrayOfAds[i].location.y + pinSize.height) + 'px');
+
+  // Создание флажка в div-блоке
+  var newImg = document.createElement('img');
+  newImg.className = 'rounded';
+  newImg.setAttribute('src', arrayOfAds[i].author.avatar);
+  newImg.setAttribute('width', pinSize.width);
+  newImg.setAttribute('height', pinSize.height);
+  newDiv.appendChild(newImg);
+
+  // Добавляю новый узел во фрагмент
+  fragment.appendChild(newDiv);
+
+  // Если данная итерация цикла первая, тогда вставить первое сгенерированное объявление в блок с id="offer-dialog" - детальное описание объявления
+  if (i === 0) {
+    // На основе шаблона генерирую новый узел
+    var newElement = templateOffer.cloneNode(true);
+
+    // Заполняю его данными из первого объявления
+    newElement.querySelector('.lodge__title').textContent = arrayOfAds[i].offer.title;
+    newElement.querySelector('.lodge__address').textContent = arrayOfAds[i].offer.address;
+    newElement.querySelector('.lodge__price').innerHTML = arrayOfAds[i].offer.price + '&#x20bd;/ночь';
+    newElement.querySelector('.lodge__type').textContent = arrayOfAds[i].getRusLodgeType(arrayOfAds[i].offer.type);
+    newElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + arrayOfAds[i].offer.guests + ' гостей в ' + arrayOfAds[i].offer.rooms + ' комнатах';
+    newElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + arrayOfAds[i].offer.checkin + ', выезд до ' + arrayOfAds[i].offer.checkout;
+    newElement.querySelector('.lodge__description').textContent = arrayOfAds[i].description;
+
+    // В цикле генерирую span-элементы обозначающие доп. опции жилища
+    var featuresNumber = arrayOfAds[i].offer.features.length;
+    for (var j = 0; j < featuresNumber; j++) {
+      var newSpan = document.createElement('span');
+      newSpan.className = 'feature__image feature__image--' + arrayOfAds[i].offer.features[j];
+      newElement.querySelector('.lodge__features').appendChild(newSpan);
+    }
+    var oldDialogPanel = document.querySelector('.dialog__panel');
+    oldDialogPanel.parentNode.replaceChild(newElement, oldDialogPanel);
+
+    // Меняю аватар в блоке с детальным описанием объявления
+    document.getElementById('offer-dialog').querySelector('.dialog__title').querySelector('img').setAttribute('src', arrayOfAds[i].author.avatar);
+  }
+}
+
+var pinMap = document.querySelector('.tokyo__pin-map');
+pinMap.appendChild(fragment)
 
 
