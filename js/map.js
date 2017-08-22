5689491959b79cef217b82e0a5957a84fccdfeef
Sadovenko Dmitriy <sadovenko.dmitry@gmail.com>
2017-08-22 14:55:18 +0300

    Правка замечаний

--- js/map.js
+++ js/map.js
@@ -37,7 +37,7 @@ var getAnyElementFisher = function (arrayOfElements) {
  * @return {*}
  */
 var getAnyElement = function (arrayOfElements) {
-  return arrayOfElements[getRandomNumber(arrayOfElements.length - 1, 1)];
+  return arrayOfElements[getRandomNumber(arrayOfElements.length - 1, 0)];
 };
 
 /**
@@ -72,6 +72,90 @@ var getFeaturesArray = function (arrayOfElements) {
 var getRusLodgeType = function (offerTypeEn) {
   return flatTypes.rus[flatTypes.en.indexOf(offerTypeEn)];
 };
+
+/**
+ * Возвращает объект-объявление
+ * @return {{author: {avatar: *}, offer: {title: *, address: string, price: number, type: *, rooms: number, guests: number, checkin: *, checkout: *, features: Array}, location: {x: number, y: number}, description: string, photos: Array}}
+ */
+var addCreateAd = function () {
+  locationX = getRandomNumber(900, 300);
+  locationY = getRandomNumber(500, 100);
+  roomsNumber = getRandomNumber(5, 1);
+
+  return {
+    author: {avatar: getAnyElementFisher(userAvatarPaths)},
+    offer: {
+      title: getAnyElementFisher(offerTitles),
+      address: locationX + ', ' + locationY,
+      price: getRandomNumber(1000000, 1000),
+      type: getAnyElement(flatTypes.en),
+      rooms: roomsNumber,
+      guests: roomsNumber * getRandomNumber(3, 1),
+      checkin: getAnyElement(checkInOutTimes),
+      checkout: getAnyElement(checkInOutTimes),
+      features: getFeaturesArray(additionalFeatures)
+    },
+    location: {x: locationX, y: locationY},
+    description: '',
+    photos: []
+  };
+};
+
+/**
+ * Создание div-блока для нового флажка
+ * @param {Object} singleAd - экземпляр объявления
+ * @param  {Object} pin - размеры пин-флажка
+ * @return {Element} - div-блок, html узел
+ */
+var createAnotherDiv = function (singleAd, pin) {
+  var newDiv = document.createElement('div');
+  newDiv.className = 'pin';
+  newDiv.setAttribute('style', 'left: ' + (singleAd.location.x + pin.width / 2) + 'px; top: ' + (singleAd.location.y + pin.height) + 'px');
+  newDiv.appendChild(createAnotherPin(singleAd));
+  return newDiv;
+};
+
+/**
+ * Создание pin-флажка в div-блоке
+ * @param {Object} singleAd - экземпляр объявления
+ * @return {Element} - флажок-пин, html узел
+ */
+var createAnotherPin = function (singleAd) {
+  var newPin = document.createElement('img');
+  newPin.className = 'rounded';
+  newPin.setAttribute('src', singleAd.author.avatar);
+  newPin.setAttribute('width', '40');
+  newPin.setAttribute('height', '40');
+  return newPin;
+};
+
+/**
+ * На основе шаблона генерирую новый узел с детальным описанием первого объявления
+ * @param {Object} singleAd - экземпляр объявления
+ * @return {Element} - html узел с детальным описанием объявления
+ */
+var createNodeWithDetailInfo = function (singleAd) {
+  var newElement = templateOffer.cloneNode(true);
+
+  // Заполняю блок данными из объявления
+  newElement.querySelector('.lodge__title').textContent = singleAd.offer.title;
+  newElement.querySelector('.lodge__address').textContent = singleAd.offer.address;
+  newElement.querySelector('.lodge__price').innerHTML = singleAd.offer.price + '&#x20bd;/ночь';
+  newElement.querySelector('.lodge__type').textContent = getRusLodgeType(singleAd.offer.type);
+  newElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + singleAd.offer.guests + ' гостей в ' + singleAd.offer.rooms + ' комнатах';
+  newElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + singleAd.offer.checkin + ', выезд до ' + singleAd.offer.checkout;
+  newElement.querySelector('.lodge__description').textContent = singleAd.description;
+
+  // В цикле генерирую span-элементы обозначающие доп. опции жилища
+  var featuresNumber = singleAd.offer.features.length;
+  for (var j = 0; j < featuresNumber; j++) {
+    var newSpan = document.createElement('span');
+    newSpan.className = 'feature__image feature__image--' + singleAd.offer.features[j];
+    newElement.querySelector('.lodge__features').appendChild(newSpan);
+  }
+  return newElement;
+};
+
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -105,6 +189,7 @@ var additionalFeatures = {
   values: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
   counter: 0
 };
+
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 var i = 0;
@@ -117,68 +202,15 @@ var roomsNumber = 0;
 
 for (i = 0; i < 8; i++) {
   // Генерация массива объявляений
+  arrayOfAds.push(addCreateAd());
 
-  locationX = getRandomNumber(900, 300);
-  locationY = getRandomNumber(500, 100);
-  roomsNumber = getRandomNumber(5, 1);
-
-  arrayOfAds.push({
-    author: {avatar: getAnyElementFisher(userAvatarPaths)},
-    offer: {
-      title: getAnyElementFisher(offerTitles),
-      address: locationX + ', ' + locationY,
-      price: getRandomNumber(1000000, 1000),
-      type: getAnyElement(flatTypes.en),
-      rooms: roomsNumber,
-      guests: roomsNumber * getRandomNumber(3, 1),
-      checkin: getAnyElement(checkInOutTimes),
-      checkout: getAnyElement(checkInOutTimes),
-      features: getFeaturesArray(additionalFeatures)
-    },
-    location: {x: locationX, y: locationY},
-    description: '',
-    photos: []
-  });
-
-  // Создание div-блока для нового флажка
-  var newDiv = document.createElement('div');
-  newDiv.className = 'pin';
-  newDiv.setAttribute('style', 'left: ' + (arrayOfAds[i].location.x + pinSize.width / 2) + 'px; top: ' + (arrayOfAds[i].location.y + pinSize.height) + 'px');
-
-  // Создание pin-флажка в div-блоке
-  var newImg = document.createElement('img');
-  newImg.className = 'rounded';
-  newImg.setAttribute('src', arrayOfAds[i].author.avatar);
-  newImg.setAttribute('width', '40');
-  newImg.setAttribute('height', '40');
-  newDiv.appendChild(newImg);
-
-  // Добавляю новый узел во фрагмент
-  fragment.appendChild(newDiv);
+  // Добавляю новый div-узел во фрагмент
+  fragment.appendChild(createAnotherDiv(arrayOfAds[i], pinSize));
 
   // Если данная итерация цикла первая, тогда вставить первое сгенерированное объявление в блок с id="offer-dialog" - детальное описание объявления
   if (i === 0) {
-    // На основе шаблона генерирую новый узел
-    var newElement = templateOffer.cloneNode(true);
-
-    // Заполняю его данными из первого объявления
-    newElement.querySelector('.lodge__title').textContent = arrayOfAds[i].offer.title;
-    newElement.querySelector('.lodge__address').textContent = arrayOfAds[i].offer.address;
-    newElement.querySelector('.lodge__price').innerHTML = arrayOfAds[i].offer.price + '&#x20bd;/ночь';
-    newElement.querySelector('.lodge__type').textContent = getRusLodgeType(arrayOfAds[i].offer.type);
-    newElement.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + arrayOfAds[i].offer.guests + ' гостей в ' + arrayOfAds[i].offer.rooms + ' комнатах';
-    newElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + arrayOfAds[i].offer.checkin + ', выезд до ' + arrayOfAds[i].offer.checkout;
-    newElement.querySelector('.lodge__description').textContent = arrayOfAds[i].description;
-
-    // В цикле генерирую span-элементы обозначающие доп. опции жилища
-    var featuresNumber = arrayOfAds[i].offer.features.length;
-    for (var j = 0; j < featuresNumber; j++) {
-      var newSpan = document.createElement('span');
-      newSpan.className = 'feature__image feature__image--' + arrayOfAds[i].offer.features[j];
-      newElement.querySelector('.lodge__features').appendChild(newSpan);
-    }
     var oldDialogPanel = document.querySelector('.dialog__panel');
-    oldDialogPanel.parentNode.replaceChild(newElement, oldDialogPanel);
+    oldDialogPanel.parentNode.replaceChild(createNodeWithDetailInfo(arrayOfAds[i]), oldDialogPanel);
 
     // Меняю аватар в блоке с детальным описанием объявления
     document.getElementById('offer-dialog').querySelector('.dialog__title').querySelector('img').setAttribute('src', arrayOfAds[i].author.avatar);
